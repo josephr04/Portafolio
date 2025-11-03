385a9447c1eadf524d2a28e490e0d8f83f82d274
josephr04 <jrosas.dev@gmail.com>
2025-11-02 21:20:39 -0500

    feat: update App component to include Header section and restructure imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import './App.css'
-import { NavBar } from './NavBar'
-import { About } from './About';
+import { NavBar } from './components/NavBar'
+import { Header } from './sections/Header';
+import { About } from './sections/About';
+import { Skills } from './sections/Skills';
+import { Education } from './sections/Education';
+import { Projects } from './sections/Projects';
 
 function App() {
 
   return (
     <>
-      <div>
+      <div className=''>
         <NavBar />
+        <Header />
         <About />
+        <Education />
+        <Skills />
+        <Projects />
       </div>
     </>
   )
