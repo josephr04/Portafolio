40b0b10914c1c026bd3a67b367309dfb258ac567
josephr04 <jrosas.dev@gmail.com>
2025-11-02 21:20:26 -0500

    feat: add Projects section with project details and links

---
+++ src/projects.ts
@@ -0,0 +1,14 @@
+export const projects = [
+    {
+      title: "Tech Store Manager",
+      image: "tecnomarket.png",
+      description: "A modern and responsive inventory management system, where store owners can track products and manage stock levels.",
+      tech: [
+        "PHP",
+        "Javascript",
+        "Bootstrap",
+      ],
+      repository: "https://github.com/josephr04/ds6-proyecto1",
+      demo: "https://github.com/josephr04/ds6-proyecto1",
+    },
+];
