3e01d210757e551c2e167495dbc61441738616b7
josephr04 <jrosas.dev@gmail.com>
2025-11-09 22:16:59 -0500

    Feat: add projects information

--- src/projects.ts
+++ src/projects.ts
@@ -11,4 +11,28 @@ export const projects = [
       repository: "https://github.com/josephr04/ds6-proyecto1",
       demo: "https://github.com/josephr04/ds6-proyecto1",
     },
+    {
+      title: "Movies+ Web Library",
+      image: "movies.png",
+      description: "Movie catalog that encompasses a wide variety of movies provided by The Movie Database (TMDB) API.",
+      tech: [
+        "Next.js",
+        "Typescript",
+        "React",
+      ],
+      repository: "https://github.com/josephr04/movie-app",
+      demo: "https://moviesplus.vercel.app",
+    },
+    {
+      title: "Flag Matching Game",
+      image: "flagmatchinggame.png",
+      description: "A simple and fun web game where players test their geography knowledge by matching countries with their flags.",
+      tech: [
+        "Typescript",
+        "React",
+        "Tailwind",
+      ],
+      repository: "https://github.com/josephr04/flag-matching-game",
+      demo: "https://flag-matching-game.vercel.app",
+    },
 ];
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -4,37 +4,39 @@ import { projects } from "../projects";
 
 export function Projects() {
   return (
-    <div className='px-[2em] pt-10 pb-10 text-white flex flex-col'>
+    <div id="projects" className='px-[2em] pt-10 pb-10 text-white flex flex-col'>
       <div className="flex flex-col items-center justify-center mb-8">
         <div className="text-center text-3xl font-bold mb-2">Projects</div>
         <div className="h-1 bg-linear-to-r from-blue-400 to-purple-500 rounded-xl w-12"></div>
       </div>
-      {projects.map((project) => (
-        <div key={project.title} className="bg-gray-800/60 rounded-2xl max-w-lg overflow-hidden flex flex-col border border-[#30363d] hover:border-blue-500 hover:-translate-y-0.5 transition-all duration-300 hover:shadow-lg hover:shadow-blue-500/20">
-          <img className="object-cover h-45 sm:h-auto" src={`../public/${project.image}`} alt={project.title} />
-          <div className="p-4">
-            <div className="text-xl font-medium mb-4 mt-2">{project.title}</div>
-            <div className="text-gray-400 mb-4">{project.description}</div>
-            <div className="flex flex-wrap gap-2 mb-6 font-semibold">
-              {project.tech.map((item) => (
-                <div key={item} className="px-3 py-1 rounded-3xl bg-linear-to-r from-blue-400/20 to-purple-500/20 text-white text-sm shadow-md text-center">
-                  <span className="bg-linear-to-r from-blue-400 to-purple-500 bg-clip-text text-transparent">{item}</span>
-                </div>
-              ))}
-            </div>
-            <div className="flex gap-3 mb-2">
-              <a className="flex gap-1 items-center" href={project.repository} rel="noopener noreferrer" target="_blank">
-                <FaGithub className="icon" size={18} color="#99a1af" />
-                <span className="text-gray-400">Code</span>
-              </a>
-              <a className="flex gap-1 items-center" href={project.demo} rel="noopener noreferrer" target="_blank">
-                <FaExternalLinkAlt className="icon" size={16} color="#99a1af" />
-                <span className="text-gray-400">Demo</span>
-              </a>
+      <div className="flex flex-wrap gap-8">
+        {projects.map((project) => (
+          <div key={project.title} className="bg-gray-800/60 rounded-2xl max-w-lg overflow-hidden flex flex-col border border-[#30363d] hover:border-blue-500 hover:-translate-y-0.5 transition-all duration-300 hover:shadow-lg hover:shadow-blue-500/20">
+            <img className="object-cover h-45 sm:h-auto" src={`../public/${project.image}`} alt={project.title} />
+            <div className="p-4">
+              <div className="text-xl font-medium mb-4 mt-2">{project.title}</div>
+              <div className="text-gray-400 mb-4">{project.description}</div>
+              <div className="flex flex-wrap gap-2 mb-6 font-semibold">
+                {project.tech.map((item) => (
+                  <div key={item} className="px-3 py-1 rounded-3xl bg-linear-to-r from-blue-400/20 to-purple-500/20 text-white text-sm shadow-md text-center">
+                    <span className="bg-linear-to-r from-blue-400 to-purple-500 bg-clip-text text-transparent">{item}</span>
+                  </div>
+                ))}
+              </div>
+              <div className="flex gap-3 mb-2">
+                <a className="flex gap-1 items-center" href={project.repository} rel="noopener noreferrer" target="_blank">
+                  <FaGithub className="icon" size={18} color="#99a1af" />
+                  <span className="text-gray-400">Code</span>
+                </a>
+                <a className="flex gap-1 items-center" href={project.demo} rel="noopener noreferrer" target="_blank">
+                  <FaExternalLinkAlt className="icon" size={16} color="#99a1af" />
+                  <span className="text-gray-400">Demo</span>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
