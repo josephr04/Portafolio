f7ef90c3cdef0504d824660059825391ebd711f3
josephr04 <jrosas.dev@gmail.com>
2025-11-24 23:13:31 -0500

    feat: create data folder to store data files

---
+++ src/data/highlights.ts
@@ -0,0 +1,24 @@
+import { FaCode, FaLaptopCode, FaRocket, FaUsers } from 'react-icons/fa';
+
+export const highlights = [
+  {
+    icon: FaCode,
+    title: "Clean Code",
+    description: "Writing maintainable and scalable solutions"
+  },
+  {
+    icon: FaLaptopCode,
+    title: "Full-Stack",
+    description: "Expertise in frontend and backend development"
+  },
+  {
+    icon: FaRocket,
+    title: "Fast Learner",
+    description: "Always eager to learn new technologies"
+  },
+  {
+    icon: FaUsers,
+    title: "Team Player",
+    description: "Excel in collaborative environments"
+  }
+  ];
